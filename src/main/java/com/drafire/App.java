c6cc51b2556bd0fd6a79692a951bfd4e22169394
pangtinghuan <pangtinghuan@tuandai.com>
2018-11-08 18:41:26 +0800

    如果要支持其他协议如hessian的时候，也要添加对应的协议的依赖。练习了多版本、异步、超时的练习

--- src/main/java/com/drafire/App.java
+++ src/main/java/com/drafire/App.java
@@ -1,27 +1,37 @@
 package com.drafire;
 
+import com.alibaba.dubbo.rpc.RpcContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) throws IOException {
-        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("order-consumer.xml");
+public class App {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("order-consumer.xml");
 
-        //用户下单过程
-        IOrderServices services=(IOrderServices)context.getBean("orderServices");
+        //同步调用过程
+        DoOrderRequest request1 = new DoOrderRequest();
+        request1.setName("drafire");
+        IOrderServices services1 = (IOrderServices) context.getBean("orderServices1");
+        DoOrderResponse response1 = services1.doOrder(request1);
+        System.out.println(response1);
 
-        DoOrderRequest request=new DoOrderRequest();
-        request.setName("drafire");
-        DoOrderResponse response=services.doOrder(request);
+        //异步调用
+        IOrderServices services2 = (IOrderServices) context.getBean("orderServices2");
+        //立即返回null
+        DoOrderResponse response2 = services2.doOrder(request1);
+        System.out.println(response2);
+        Future<DoOrderResponse> future2 = RpcContext.getContext().getFuture();
+        //阻塞直到services2.doOrder(request1) 返回
+        response2 = future2.get();
+        System.out.println(response2);
 
-        System.out.println(response);
-
+        System.out.println("全搞定");
         //Order.doOrder();
         System.in.read();
     }
