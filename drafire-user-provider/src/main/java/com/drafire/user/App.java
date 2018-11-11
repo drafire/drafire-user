00c4c7f77eb5499e0a2ad9067dedacd20947d6a6
drafire <hyljljlj@163.com>
2018-11-11 23:08:45 +0800

    把端口号修改成20881

---
+++ drafire-user-provider/src/main/java/com/drafire/user/App.java
@@ -0,0 +1,16 @@
+package com.drafire.user;
+
+import com.alibaba.dubbo.container.Main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Main.main(args);
+        System.out.println("執行完了");
+    }
+}
