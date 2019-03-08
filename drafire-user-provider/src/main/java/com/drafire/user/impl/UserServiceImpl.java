3f60fd3c4ec22d88c6f20bacfc3c35f541b82910
pangtinghuan <pangtinghuan@tuandai.com>
2019-03-08 17:37:45 +0800

    测试了zookeeper节点性质

--- drafire-user-provider/src/main/java/com/drafire/user/impl/UserServiceImpl.java
+++ drafire-user-provider/src/main/java/com/drafire/user/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service("userServide")
 public class UserServiceImpl implements IUserService {
+
     @Override
     public LoginResponse login(LoginRequest loginRequest) {
         //参数校验
